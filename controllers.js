ce3d62cb52cde6f8ea6b2bad38685959a5122de2
user1 <user1@localhost.localdomain>
2018-03-10 19:08:53 -0500

    Upgrade All Project

--- controllers.js
+++
@@ -1,9 +0,0 @@
-exports.sendLogin = function (req, res) {
-    res.render('./public/index.html');
-};
-exports.sendSign = function (req, res) {
-    res.render('./public/sign.html');
-};
-exports.sendMapa = function (req, res) {
-    res.render('./public/mapa.html');
-};
